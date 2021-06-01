db223cdef4d408e5f1a7cb08315c915d1553b91a
nathanRomad <nathan.a.hamilton@gmail.com>
2021-06-01 15:40:41 -0500

    refactor: removed comments from customer.py model

--- bangazonapi/models/customer.py
+++ bangazonapi/models/customer.py
@@ -7,8 +7,6 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING,)
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=55)
-    # recommends = 
-    # recommended =
 
     @property
     def recommends(self):
