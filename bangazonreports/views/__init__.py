8e69aa6b6997e0600b1904ca543d59d761ffd574
nathanRomad <nathan.a.hamilton@gmail.com>
2021-06-02 10:56:20 -0500

    fix [bangazonreports]: added HTML report for favorite sellers by customer.

---
+++ bangazonreports/views/__init__.py
@@ -0,0 +1,2 @@
+from .connection import Connection
+from .users.favsellerbycustomer import favseller_list
--- bangazonreports/views/users/favsellerbycustomer.py
+++ bangazonreports/views/users/favsellerbycustomer.py
@@ -1,7 +1,7 @@
 """Module for generating games by user report"""
 import sqlite3
 from django.shortcuts import render
-from bangazonapi.models import 
+from bangazonapi.models import Customer
 from bangazonreports.views import Connection
 
 
@@ -18,52 +18,51 @@ def favseller_list(request):
                 SELECT
                     c.id custId,
                     u.first_name || ' ' || u.last_name AS fullName,
-                    u.username,
-                    fav.id favId
+                    fav.seller_id seller,
+                    u2.first_name || '' || u2.last_name AS sellerName
                 FROM
                     bangazonapi_customer c
                 JOIN
                     bangazonapi_favorite fav ON c.id = fav.customer_id
                 JOIN
                     auth_user u ON c.id = u.id
+                JOIN 
+                    auth_user u2 ON seller = u2.id
             """)
 
             dataset = db_cursor.fetchall()
 
-            games_by_user = {}
+            favseller_by_cust = {}
 
             for row in dataset:
                 # Crete a Game instance and set its properties
-                game = Game()
-                game.title = row["title"]
-                game.maker = row["maker"]
-                game.skill_level = row["skill_level"]
-                game.number_of_players = row["number_of_players"]
-                game.gametype_id = row["gametype_id"]
+                favorite = Customer()
+                favorite.id = row['seller']
+                favorite.name = row['sellerName']
 
                 # Store the user's id
-                uid = row["user_id"]
+                uid = row["custId"]
 
                 # If the user's id is already a key in the dictionary...
-                if uid in games_by_user:
+                if uid in favseller_by_cust:
 
                     # Add the current game to the `games` list for it
-                    games_by_user[uid]['games'].append(game)
+                    favseller_by_cust[uid]['seller'].append(favorite)
 
                 else:
                     # Otherwise, create the key and dictionary value
-                    games_by_user[uid] = {}
-                    games_by_user[uid]["id"] = uid
-                    games_by_user[uid]["full_name"] = row["full_name"]
-                    games_by_user[uid]["games"] = [game]
+                    favseller_by_cust[uid] = {}
+                    favseller_by_cust[uid]["id"] = uid
+                    favseller_by_cust[uid]["fullName"] = row["fullName"]
+                    favseller_by_cust[uid]["seller"] = [favorite]
 
         # Get only the values from the dictionary and create a list from them
-        list_of_users_with_games = games_by_user.values()
+        list_of_users_with_favs = favseller_by_cust.values()
 
         # Specify the Django template and provide data context
-        template = 'users/list_with_games.html'
+        template = 'users/favsellerbycust.html'
         context = {
-            'usergame_list': list_of_users_with_games
+            'favseller_list': list_of_users_with_favs
         }
 
         return render(request, template, context)
